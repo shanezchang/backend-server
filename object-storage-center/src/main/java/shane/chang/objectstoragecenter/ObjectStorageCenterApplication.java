315dc6d9b436d92f32d7cd61468cdd922f6d956f
shane <shane.z.chang@gmail.com>
2024-05-05 21:29:29 +0800

    feat: init proj

---
+++ object-storage-center/src/main/java/shane/chang/objectstoragecenter/ObjectStorageCenterApplication.java
@@ -0,0 +1,13 @@
+package shane.chang.objectstoragecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectStorageCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectStorageCenterApplication.class, args);
+	}
+
+}
---
+++ object-storage-center/src/test/java/shane/chang/objectstoragecenter/ObjectStorageCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package shane.chang.objectstoragecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectStorageCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
