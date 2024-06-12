a5ac23382efa0c7d91fe26413087bce499759fe4
shane <shane.z.chang@gmail.com>
2024-06-12 10:54:50 +0800

    feat: init notify center

---
+++ notify-center/src/test/java/com/example/notify_center/NotifyCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notify_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
