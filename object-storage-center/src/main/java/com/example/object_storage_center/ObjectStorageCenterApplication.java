0716ad2cf7c73efd562cf9a46c11e9102854ed09
shane <shane.z.chang@gmail.com>
2024-06-18 18:37:26 +0800

    feat: osc 部署到 云服务器

--- object-storage-center/src/main/java/com/example/object_storage_center/ObjectStorageCenterApplication.java
+++ object-storage-center/src/main/java/com/example/object_storage_center/ObjectStorageCenterApplication.java
@@ -2,8 +2,14 @@ package com.example.object_storage_center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@RefreshScope
 public class ObjectStorageCenterApplication {
 
 	public static void main(String[] args) {
