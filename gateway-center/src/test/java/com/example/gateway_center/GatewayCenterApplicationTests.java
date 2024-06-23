f6402910549c84ce13036965d4cc4fe99d16a208
shane <shane.z.chang@gmail.com>
2024-06-23 16:40:06 +0800

    feat: add gateway

---
+++ gateway-center/src/test/java/com/example/gateway_center/GatewayCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gateway_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- notify-center/src/main/java/com/example/notify_center/NotifyCenterApplication.java
+++ notify-center/src/main/java/com/example/notify_center/NotifyCenterApplication.java
@@ -3,11 +3,13 @@ package com.example.notify_center;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@RefreshScope
 public class NotifyCenterApplication {
 
 	public static void main(String[] args) {
